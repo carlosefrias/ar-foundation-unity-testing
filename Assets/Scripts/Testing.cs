1662a13a32d3d70a7d67426d2868165adb6d8905
Carlos Frias <carlosefrias@gmail.com>
2019-07-11 11:15:02 +0100

    starting folowing https://www.youtube.com/watch?v=Ml2UakwRxjk tutorial

---
+++ Assets/Scripts/Testing.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hello world");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
